7fce4beb8f2cf1c6bd241959a5f529b17d08da97
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-05-06 12:42:13 +0200

    feat(settings): add connection to old database

--- apis_instance_viecpro/settings.py
+++ apis_instance_viecpro/settings.py
@@ -1,6 +1,7 @@
 # 'INSTALLED_APPS' may be undefined, or defined from star imports: apis_acdhch_default_settings.settings
 # ruff: noqa: F405
 from apis_acdhch_default_settings.settings import *  # noqa: F403
+import os
 
 INSTALLED_APPS[INSTALLED_APPS.index("apis_ontology")] = "apis_instance_viecpro"
 INSTALLED_APPS += ["simple_history"]
@@ -9,3 +10,7 @@ INSTALLED_APPS += ["django.contrib.postgres"]
 ROOT_URLCONF = "apis_instance_viecpro.urls"
 
 LANGUAGE_CODE = "de"
+
+if db_url := os.environ.get("OLD_DATABASE_URL", False):
+    print("Connecting to OLD_DATABASE_URL")
+    DATABASES["old"] = dj_database_url.parse(db_url)
