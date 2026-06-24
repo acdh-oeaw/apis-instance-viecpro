32f28d7ad757aaf7ee21902b1472353fbc270c25
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-06-24 07:35:34 +0200

    fix(models): adapt imports for new apis-core-rdf version

--- apis_instance_viecpro/models.py
+++ apis_instance_viecpro/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from apis_core.apis_entities.abc import E53_Place, E21_Person, E74_Group
+from apis_core.entities.abc import E53_Place, E21_Person, E74_Group
 from apis_core.history.models import VersionMixin
 from apis_core.relations.models import Relation
 from apis_core.entities.abc import Entity
@@ -7,7 +7,7 @@ from apis_core.generic.abc import GenericModel
 from django_interval.fields import FuzzyDateParserField
 
 
-class Person(VersionMixin, Entity, E21_Person):
+class Person(VersionMixin, E21_Person):
     diverses = models.TextField()
     ereignisdatum = models.TextField()
     quellenzitat = models.TextField()
@@ -17,13 +17,13 @@ class Person(VersionMixin, Entity, E21_Person):
     merged_into = models.IntegerField(editable=False)
 
 
-class Institution(VersionMixin, Entity, E74_Group): ...
+class Institution(VersionMixin, E74_Group): ...
 
 
 class Event(VersionMixin, Entity): ...
 
 
-class Place(VersionMixin, Entity, E53_Place): ...
+class Place(VersionMixin, E53_Place): ...
 
 
 class Payment(VersionMixin, Entity): ...
