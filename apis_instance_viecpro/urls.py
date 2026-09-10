1e51a3b32c2970cde9d4d660298b5fce56354b12
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-09-10 07:51:53 +0200

    feat(urls): enable django grouper urls

--- apis_instance_viecpro/urls.py
+++ apis_instance_viecpro/urls.py
@@ -2,3 +2,4 @@ from apis_acdhch_default_settings.urls import urlpatterns
 from django.urls import include, path
 
 urlpatterns += [path("", include("django_interval.urls"))]
+urlpatterns += [path("", include("django_grouper.urls"))]
