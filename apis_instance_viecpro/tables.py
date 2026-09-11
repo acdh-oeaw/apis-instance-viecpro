2b0105b6c76bd3de53d2b6efe9a91aee1689cd7a
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-09-11 08:20:32 +0200

    feat(tables): add start, end, label & notes to the relation tables
    Closes: #64

--- apis_instance_viecpro/tables.py
+++ apis_instance_viecpro/tables.py
@@ -1,3 +1,4 @@
+import django_tables2 as tables
 from apis_core.generic.tables import CustomTemplateColumn
 from apis_core.relations.tables import RelationsListTable
 
@@ -8,6 +9,10 @@ class ReferenceColumn(CustomTemplateColumn):
 
 class EntityRelationsTable(RelationsListTable):
     reference = ReferenceColumn()
+    start = tables.Column(order_by="start_date_sort")
+    end = tables.Column(order_by="end_date_sort")
+    label = tables.Column(accessor="legacy_relation_vocab_label")
+    notes = tables.Column()
 
     class Meta(RelationsListTable.Meta):
         sequence = RelationsListTable.Meta.sequence + ("reference",)
